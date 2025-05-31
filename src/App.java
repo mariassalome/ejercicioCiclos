dc3c7c2d17718a03fd6b1c4f74c931cccd22671d
salome888-code <salomealzate888@gmail.com>
2025-05-31 14:12:03 -0500

    Se finaliza el ejercicio

--- src/App.java
+++ src/App.java
@@ -16,15 +16,48 @@ public class App {
             if (usuario.equals("admin") && contrasena.equals("123")) {
                 System.out.println("Bienvenido al Sistema de notas CESDE");
                 intentos = 0;
+
                 System.out.println("Seleccione: \n1 - Profe\n2 - Estudiante");
                 String opcion = sc.nextLine();
+
                 switch (opcion) {
                     case "1":
-                            System.out.println("Hola profe");
-                        break;
-                    case "2":
+                        int interruptor = 1;
+                        int cantidadEstudiantes = 0;
+                        int ganaron = 0;
+                        while (interruptor == 1) {
+                            double nota = 0;
+                            double notaFinal = 0;
+                            double sumaDeNotas = 0;
+                            for (int i = 1; i <= 3; i++) {
+                                System.out.println("Ingresando la nota: " + i);
+                                do {
+                                    nota = sc.nextDouble();
+
+                                } while (nota < 0 || nota > 5);
+                                sumaDeNotas += nota;
+                            }
+                            notaFinal = sumaDeNotas / 3;
+                            if (notaFinal >= 3) ;
+                            {
+                                ganaron++;
+                            }
+                            System.out.println("La nota final es: " + notaFinal);
+                            cantidadEstudiantes++;
+                            System.out.println("Desea ingresar la nota de otro estudiante? Si/No");
+                            opcion = sc.next();
+                            if (opcion.equals("No")) {
+                                interruptor = 0;
+
+                            }
+                        }
+                        System.out.println("Resultado de la materia Lógica de programación: ");
+                        System.out.println("Cantidad de estudiantes que ganaron: " + ganaron);
+                        System.out.println("Cantidad de estudiantes que perdieron: " + (cantidadEstudiantes - ganaron));
+                            break;
+                            case "2":
                             System.out.println("Hola estudiante");
-                        break;
+
     
                 }
             } else {
@@ -36,3 +69,4 @@ public class App {
 
     }
 }
+
